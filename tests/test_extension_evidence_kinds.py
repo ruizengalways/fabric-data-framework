14009ebe0c294ae2afac4a21866ad315999d6fb9
Rui Zeng <ruizeng.always@outlook.com>
2026-08-30 08:49:04 +1000

    test: pin Warehouse mutation entry point group

--- tests/test_extension_evidence_kinds.py
+++ tests/test_extension_evidence_kinds.py
@@ -1,7 +1,7 @@
 from fabric_data_framework.extensions import ExtensionKind
 
 
-def test_capture_evidence_extension_kinds_have_stable_entry_point_groups():
+def test_approved_evidence_extension_kinds_have_stable_entry_point_groups():
     assert (
         ExtensionKind.CAPTURE_OBSERVER.entry_point_group
         == "fabric_data_framework.capture_observers"
@@ -9,4 +9,8 @@ def test_capture_evidence_extension_kinds_have_stable_entry_point_groups():
     assert (
         ExtensionKind.SPARK_EXECUTION_DATA.entry_point_group
         == "fabric_data_framework.spark_execution_data"
+    )
+    assert (
+        ExtensionKind.WAREHOUSE_MUTATION.entry_point_group
+        == "fabric_data_framework.warehouse_mutations"
     )
